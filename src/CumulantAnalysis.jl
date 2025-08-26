cd6263251cd9e3ae587cbed9ca30c2e90716d415
Ethan Meitz <54505069+ejmeitz@users.noreply.github.com>
2025-08-26 14:21:48 -0400

    static warn on TDEP load

--- src/CumulantAnalysis.jl
+++ src/CumulantAnalysis.jl
@@ -15,6 +15,10 @@ using FileIO
 using Printf
 using OrderedCollections
 
+@static if !CumulantAnalysis.TDEP.TDEP_jll.is_available()
+    @warn "Could not load TDEP on this platform. Try Linux or MacOs"
+end
+
 const FREQ_TOL = 1e-6
 const kB = ustrip(u"eV / K", Unitful.k)
 const ħ = ustrip(u"eV * s", Unitful.ħ)
