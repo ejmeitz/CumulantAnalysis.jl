8a4ff5c80507bf6b085a5a8376e41f87ceff7dd9
Ethan Meitz <54505069+ejmeitz@users.noreply.github.com>
2025-09-03 13:12:58 -0400

    delete a lot

--- src/CumulantAnalysis.jl
+++ src/CumulantAnalysis.jl
@@ -23,14 +23,11 @@ const FREQ_TOL = 1e-6
 const kB = ustrip(u"eV / K", Unitful.k)
 const ħ = ustrip(u"eV * s", Unitful.ħ)
 
-include("lammps_dump_parser.jl")
 include("types.jl")
 include("harmonic_properties.jl")
 include("estimate.jl")
-include("block_averaging.jl")
 include("error_analysis.jl")
 include("cumulant_corrections.jl")
-include("phonon.jl")
 include("effective_hamiltonian.jl")
 
 end # CumulantAnalysis
--- src/types.jl
+++ src/types.jl
@@ -1,49 +1,25 @@
-export sTDEPEstimator, LAMMPSEstimator, MollyEstimator, estimate, Quantum, Classical
+export Quantum, Classical
 
 abstract type Limit end
 struct Quantum <: Limit end
 struct Classical <: Limit end
 
+abstract type CumulantEstimator end
 
-abstract type ThermoEstimator{O, L <: Limit}end
-abstract type MDEstimator{O} <: ThermoEstimator{O, Classical} end
-
-order(::ThermoEstimator{O}) where O = O
-
-"""
-"""
-struct sTDEPEstimator{O,L,T,C} <: ThermoEstimator{O,L}
-    cc::C # C will be CanonicalConfiguratoin
-    temperature::T
+struct EffectiveHamiltonian{T} <: CumulantEstimator
+    ifc2_path::String
+    ifc3_path::String
+    ifc4_path::String
+    U0::T
 end
 
-function sTDEPEstimator(order::Int, nsamples::Int, temperature_K, quantum::Bool)
-    limit = (quantum == true) ? Quantum : Classical
-    T = ustrip(temperature_K) * u"K"
-    cc = CanonicalConfiguration(
-        temperature = Float64(ustrip(temperature_K)),
-        nconf = nsamples,
-        quantum = quantum, 
-    )
-    return sTDEPEstimator{order, limit, typeof(T), typeof(cc)}(cc, T)
+struct HarmonicEstimator{T} <: CumulantEstimator
+    ifc2_path::String
+    U0::T
 end
 
-limit(::sTDEPEstimator{O,L}) where {O,L} = L
-stochastic(::sTDEPEstimator) = true
-
-"""
-"""
-struct LAMMPSEstimator{O,T} <: MDEstimator{O}
-    temperature::T
+struct ResidualEstimator <: CumulantEstimator
+    ifc2_path::String
+    ifc3_path::String
+    ifc4_path::String
 end
-
-
-"""
-"""
-struct MollyEstimator{O,T} <: MDEstimator{O}
-    temperature::T
-    n_configs::Int
-end
-
-limit(::MDEstimator) = Classical
-stochastic(::MDEstimator) = false
