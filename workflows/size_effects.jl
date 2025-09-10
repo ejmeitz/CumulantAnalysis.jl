a6bafa26e83c8d5acd06eb462b8f66e7e08a3df8
Ethan Meitz <54505069+ejmeitz@users.noreply.github.com>
2025-09-10 14:04:21 -0400

    update size effects script

--- workflows/size_effects.jl
+++ workflows/size_effects.jl
@@ -5,11 +5,13 @@ using SimpleCrystals
 using SimpleCrystals
 
 
-expansion_order = 3
+expansion_order = 2
+limit = Classical()
 # just pick a big number, havent looked at effect of nsteps yet
 # probably needs to be bigger for LJ than SW
-stdep_samples = 50_000 
-is_quantum = false
+samples = 100_000 
+n_boot = 500
+boot_size = 10_000
 
 # Lennard-Jones Argon
 # make_outpath = (T,s) -> "/mnt/merged/emeitz/CumulantAnalysisTest/sTDEP_LJ_size_effects/T$(ustrip(T))/$(s)UC"
@@ -25,7 +27,10 @@ make_outpath = (T,s) -> "/mnt/merged/emeitz/CumulantAnalysisTest/sTDEP_SW_size_e
 temperatures = [100, 1300]
 sizes = [2,3,4,5,6,7]
 ucposcar_path = "/home/emeitz/scripts/TDEP/SW/infile.ucposcar2"
-make_ifc_path = (T) -> "/mnt/merged/emeitz/SW_IFC_NODES/IFCs/T$(T)_0/infile.forceconstant"
+ifc2_path = (T) -> "/mnt/merged/emeitz/SW_IFC_NODES/IFCs/T$(T)_0/infile.forceconstant"
+ifc3_path = (T) -> "/mnt/merged/emeitz/SW_IFC_NODES/IFCs/T$(T)_0/infile.forceconstant_thirdorder"
+ifc4_path = (T) -> "/mnt/merged/emeitz/SW_IFC_NODES/IFCs/T$(T)_0/infile.forceconstant_fourthorder"
+U0 = Dict(100 => -4.3365637, 1300 => -4.3247041)
 
 sw_pot = "/home/emeitz/software/lammps/potentials/Si.sw"
 pot_cmds = ["pair_style sw", "pair_coeff * * \"$(sw_pot)\" Si"]
@@ -36,34 +41,39 @@ LAMMPS.MPI.Init()
 for T in temperatures
     for s in sizes
 
+        crys = make_crystal(s)
+
+        e = EffectiveHamiltonianEstimator(
+            expansion_order,
+            limit, 
+            ifc2_path(T),
+            ifc3_path(T),
+            ifc4_path(T),
+            U0[T] * length(crys),
+            samples,
+            n_boot,
+            boot_size
+        )
+
+
         outpath = make_outpath(T,s)
 
         @info "Temperature: $(T), Supercell: $(s)x$(s)x$(s)"
         mkpath(outpath)
 
-        crys = make_crystal(s)
-
         ssposcar_path = joinpath(outpath, "infile.ssposcar")
         to_ssposcar(crys, ssposcar_path)
 
-        s = TDEPSystem(ssposcar_path)
-        calc = LAMMPSCalculator(s, pot_cmds)
-
-        se = sTDEPEstimator(expansion_order, stdep_samples, T, is_quantum)
+        # s = TDEPSystem(ssposcar_path)
+        # calc = LAMMPSCalculator(s, pot_cmds)
         
-        F_c, S_c, U_c, Cv_c = estimate(
-            se,
-            calc,
+        estimate(
+            e,
+            T,
             outpath;
             ucposcar_path = ucposcar_path,
             ssposcar_path = ssposcar_path,
-            ifc_path = make_ifc_path(T),
-            n_boot = 200,
         )
 
-        save_errors(F_c,  outpath)
-        save_errors(S_c,  outpath)
-        save_errors(U_c,  outpath)
-        save_errors(Cv_c, outpath)
     end
 end
