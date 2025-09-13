095238a32ebaa9932f19a8c5b57ca622fb537b66
Ethan Meitz <54505069+ejmeitz@users.noreply.github.com>
2025-09-13 14:32:18 -0400

    do LJ

--- workflows/size_effects.jl
+++ workflows/size_effects.jl
@@ -14,28 +14,31 @@ n_boot = 500
 boot_size = 10_000
 
 # Lennard-Jones Argon
-# make_outpath = (CT, T,s) -> "/mnt/merged/emeitz/CumulantAnalysisTest/LJ_size_effects_$(CT)/T$(ustrip(T))/$(s)UC"
-# temperatures = [10, 80]
-# sizes = [3,4,5,6,7]
-# U0 = Dict(10 => -0.0777399947, 80 => -0.0780397902)
-# ucposcar_path = "/home/emeitz/scripts/TDEP/LJ/infile.ucposcar_oneatom"
-# make_ifc_path = (T) -> "/mnt/merged/emeitz/LJ_IFC_INTERPOLATION_NODES_FINE/IFCs/T$(T)_0/infile.forceconstant"
-# pot_cmds = ["pair_style lj/cut 8.5", "pair_coeff * * 0.010423 3.4", "pair_modify shift yes"]
-# make_crystal = (s) -> FCC(5.2468u"angstrom", :Ar, SVector(s,s,s))
+make_outpath = (CT, T,s) -> "/mnt/merged/emeitz/CumulantAnalysisTest/LJ_size_effects_$(CT)/T$(ustrip(T))/$(s)UC"
+temperatures = [10, 80]
+sizes = [3,4,5,6,7]
+U0 = Dict(10 => -0.0777399947, 80 => -0.0780397902)
+ucposcar_path = "/home/emeitz/scripts/TDEP/LJ/infile.ucposcar_oneatom"
+ifc2_path = (T) -> "/mnt/merged/emeitz/LJ_IFC_INTERPOLATION_NODES_FINE/IFCs/T$(T)_0/infile.forceconstant"
+ifc3_path = (T) -> "/mnt/merged/emeitz/LJ_IFC_INTERPOLATION_NODES_FINE/IFCs/T$(T)_0/infile.forceconstant_thirdorder"
+ifc4_path = (T) -> "/mnt/merged/emeitz/LJ_IFC_INTERPOLATION_NODES_FINE/IFCs/T$(T)_0/infile.forceconstant_fourthorder"
+
+pot_cmds = ["pair_style lj/cut 8.5", "pair_coeff * * 0.010423 3.4", "pair_modify shift yes"]
+make_crystal = (s) -> FCC(5.2468u"angstrom", :Ar, SVector(s,s,s))
 
 # Stillinger-Weber Silicon
-make_outpath = (CT, T,s) -> "/mnt/merged/emeitz/CumulantAnalysisTest/SW_size_effects_$(CT)/T$(ustrip(T))/$(s)UC"
-temperatures = [100, 1300]
-sizes = [2,3,4,5,6]
-ucposcar_path = "/home/emeitz/scripts/TDEP/SW/infile.ucposcar2"
-ifc2_path = (T) -> "/mnt/merged/emeitz/SW_IFC_NODES/IFCs/T$(T)_0/infile.forceconstant"
-ifc3_path = (T) -> "/mnt/merged/emeitz/SW_IFC_NODES/IFCs/T$(T)_0/infile.forceconstant_thirdorder"
-ifc4_path = (T) -> "/mnt/merged/emeitz/SW_IFC_NODES/IFCs/T$(T)_0/infile.forceconstant_fourthorder"
-U0 = Dict(100 => -4.3365637, 1300 => -4.3247041)
+# make_outpath = (CT, T,s) -> "/mnt/merged/emeitz/CumulantAnalysisTest/SW_size_effects_$(CT)/T$(ustrip(T))/$(s)UC"
+# temperatures = [100, 1300]
+# sizes = [2,3,4,5,6]
+# ucposcar_path = "/home/emeitz/scripts/TDEP/SW/infile.ucposcar2"
+# ifc2_path = (T) -> "/mnt/merged/emeitz/SW_IFC_NODES/IFCs/T$(T)_0/infile.forceconstant"
+# ifc3_path = (T) -> "/mnt/merged/emeitz/SW_IFC_NODES/IFCs/T$(T)_0/infile.forceconstant_thirdorder"
+# ifc4_path = (T) -> "/mnt/merged/emeitz/SW_IFC_NODES/IFCs/T$(T)_0/infile.forceconstant_fourthorder"
+# U0 = Dict(100 => -4.3365637, 1300 => -4.3247041)
 
-sw_pot = "/home/emeitz/software/lammps/potentials/Si.sw"
-pot_cmds = ["pair_style sw", "pair_coeff * * \"$(sw_pot)\" Si"]
-make_crystal = (s) -> Diamond(5.43u"angstrom", :Si, SVector(s,s,s))
+# sw_pot = "/home/emeitz/software/lammps/potentials/Si.sw"
+# pot_cmds = ["pair_style sw", "pair_coeff * * \"$(sw_pot)\" Si"]
+# make_crystal = (s) -> Diamond(5.43u"angstrom", :Si, SVector(s,s,s))
 
 names_dict = Dict(
     EffectiveHamiltonianEstimator => "MDV0",
