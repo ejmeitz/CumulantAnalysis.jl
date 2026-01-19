d0259ca1f4ec595711ad56a5b571a402fe69149f
Ethan Meitz <54505069+ejmeitz@users.noreply.github.com>
2026-01-19 13:06:34 -0500

    fix patha gain

--- workflows/neon.jl
+++ workflows/neon.jl
@@ -11,9 +11,9 @@ getoutpath = (T) -> joinpath(base_outpath,  "T$(T)")
 Ts = [4,6,8,10,12,14,16,18,20,22,24]
 ucposcar_path = (T) -> "/mnt/merged/emeitz/Neon_sTDEP/results/T$(T)/infile.ucposcar"
 ssposcar_path = (T) -> "/mnt/merged/emeitz/Neon_sTDEP/results/T$(T)/infile.ssposcar"
-ifc2_path = (T) -> "/mnt/merged/emeitz/Neon_sTDEP/IFCs/T$(T)/infile.forceconstant"
-ifc3_path = (T) -> "/mnt/merged/emeitz/Neon_sTDEP/IFCs/T$(T)/infile.forceconstant_thirdorder"
-ifc4_path = (T) -> "/mnt/merged/emeitz/Neon_sTDEP/IFCs/T$(T)/infile.forceconstant_fourthorder"
+ifc2_path = (T) -> "/mnt/merged/emeitz/Neon_sTDEP/results/T$(T)/infile.forceconstant"
+ifc3_path = (T) -> "/mnt/merged/emeitz/Neon_sTDEP/results/T$(T)/infile.forceconstant_thirdorder"
+ifc4_path = (T) -> "/mnt/merged/emeitz/Neon_sTDEP/results/T$(T)/infile.forceconstant_fourthorder"
 pot_cmds = ["pair_style lj/cut 6.955", "pair_coeff * * 0.0032135 2.782", "pair_modify shift yes"]
 
 
