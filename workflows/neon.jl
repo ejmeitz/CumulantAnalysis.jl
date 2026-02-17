a2733006b3ddbb5c57bde897e9ae4164011585b2
Ethan Meitz <54505069+ejmeitz@users.noreply.github.com>
2026-02-17 14:54:16 -0500

    fix structures

--- workflows/neon.jl
+++ workflows/neon.jl
@@ -5,16 +5,28 @@ nconf = 100_000
 nboot = 5000
 
 quantum = true
-base_outpath = "/mnt/merged/emeitz/CumulantAnalysisTest/Neon_ANALYTICAL_TEST"
+base_outpath = "/mnt/merged/emeitz/CumulantAnalysisTest/Neon_ANALYTICAL_PIMD"
 getoutpath = (T) -> joinpath(base_outpath,  "T$(T)")
 
 Ts = [4,6,8,10,12,14,16,18,20,22,24]
-ucposcar_path = (T) -> "/mnt/merged/emeitz/Neon_sTDEP/results/T$(T)/infile.ucposcar"
-ssposcar_path = (T) -> "/mnt/merged/emeitz/Neon_sTDEP/results/T$(T)/infile.ssposcar"
-ifc2_path = (T) -> "/mnt/merged/emeitz/Neon_sTDEP/results/T$(T)/infile.forceconstant"
-ifc3_path = (T) -> "/mnt/merged/emeitz/Neon_sTDEP/results/T$(T)/infile.forceconstant_thirdorder"
-ifc4_path = (T) -> "/mnt/merged/emeitz/Neon_sTDEP/results/T$(T)/infile.forceconstant_fourthorder"
 pot_cmds = ["pair_style lj/cut 6.955", "pair_coeff * * 0.0032135 2.782", "pair_modify shift yes"]
+
+## sTDEP IFCs
+# ifc_basepath = (T) -> "/mnt/merged/emeitz/Neon_sTDEP/results/T$(T)"
+# ucposcar_path = (T) -> joinpath(ifc_basepath(T), "infile.ucposcar")
+# ssposcar_path = (T) -> joinpath(ifc_basepath(T), "infile.ssposcar")
+# ifc2_path = (T) -> joinpath(ifc_basepath(T), "infile.forceconstant")
+# ifc3_path = (T) -> joinpath(ifc_basepath(T), "infile.forceconstant_thirdorder")
+# ifc4_path = (T) -> joinpath(ifc_basepath(T), "infile.forceconstant_fourthorder")
+
+
+## PIMD IFCs
+ifc_basepath = (T) -> "/mnt/merged/emeitz/Neon_PIMD_IFCs/collected_outputs/T$(T)"
+ucposcar_path = (T) -> joinpath(ifc_basepath(T), "infile.ucposcar")
+ssposcar_path = (T) -> joinpath(ifc_basepath(T), "infile.ssposcar")
+ifc2_path = (T) -> joinpath(ifc_basepath(T), "infile.forceconstant")
+ifc3_path = (T) -> joinpath(ifc_basepath(T), "infile.forceconstant_thirdorder")
+ifc4_path = (T) -> joinpath(ifc_basepath(T), "infile.forceconstant_fourthorder")
 
 crystal_thermodynamic_properties(
     Ts,
