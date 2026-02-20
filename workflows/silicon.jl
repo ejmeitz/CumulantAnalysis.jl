c5f0c31a8c43d0899dd35773432abda9f5a25bf1
Ethan Meitz <54505069+ejmeitz@users.noreply.github.com>
2026-02-20 12:39:38 -0500

    update silicon script

--- workflows/silicon.jl
+++ workflows/silicon.jl
@@ -4,19 +4,28 @@ nconf = 100_000
 nboot = 5_000
 
 quantum = false
-base_outpath = "/mnt/merged/emeitz/CumulantAnalysisTest/SW_ANALYTICAL"
+base_outpath = "/mnt/merged/emeitz/CumulantAnalysisTest/SW_ANALYTICAL_sTDEP"
 getoutpath = (T) -> joinpath(base_outpath,  "T$(T)")
 
-pot = "SW"
-Ts = [100,300,500,700,900,1100,1300]
-ucposcar_path = raw"/mnt/merged/emeitz/SW_IFC_NODES/infile.ucposcar"
-ssposcar_path = raw"/mnt/merged/emeitz/SW_IFC_NODES/infile.ssposcar" #3UC
-ifc2_path = (T) -> "/mnt/merged/emeitz/SW_IFC_NODES/IFCs/T$(T)_0/infile.forceconstant"
-ifc3_path = (T) -> "/mnt/merged/emeitz/SW_IFC_NODES/IFCs/T$(T)_0/infile.forceconstant_thirdorder"
-ifc4_path = (T) -> "/mnt/merged/emeitz/SW_IFC_NODES/IFCs/T$(T)_0/infile.forceconstant_fourthorder"
-
+Ts = [100,200,300,400,500,600,700,800,900,1000,1100,1200,1300]
 sw_pot = "/home/emeitz/software/lammps/potentials/Si.sw"
 pot_cmds = ["pair_style sw", "pair_coeff * * \"$(sw_pot)\" Si"]
+
+# MD-TDEP IFCs
+# ucposcar_path = raw"/mnt/merged/emeitz/SW_IFC_NODES/infile.ucposcar"
+# ssposcar_path = raw"/mnt/merged/emeitz/SW_IFC_NODES/infile.ssposcar" #3UC
+# ifc2_path = (T) -> "/mnt/merged/emeitz/SW_IFC_NODES/IFCs/T$(T)_0/infile.forceconstant"
+# ifc3_path = (T) -> "/mnt/merged/emeitz/SW_IFC_NODES/IFCs/T$(T)_0/infile.forceconstant_thirdorder"
+# ifc4_path = (T) -> "/mnt/merged/emeitz/SW_IFC_NODES/IFCs/T$(T)_0/infile.forceconstant_fourthorder"
+
+# sTDEP IFCs
+stdep_basepath = (T) -> "/mnt/merged/emeitz/SW_sTDEP/RESULTS/T$(T)_0"
+ucposcar_path = (T) -> joinpath(stdep_basepath(T), "infile.ucposcar")
+ssposcar_path = (T) -> joinpath(stdep_basepath(T), "infile.ssposcar")
+ifc2_path = (T) -> joinpath(stdep_basepath(T), "infile.forceconstant")
+ifc3_path = (T) -> joinpath(stdep_basepath(T), "infile.forceconstant_thirdorder")
+ifc4_path = (T) -> joinpath(stdep_basepath(T), "infile.forceconstant_fourthorder")
+
 
 crystal_thermodynamic_properties(
     Ts,
