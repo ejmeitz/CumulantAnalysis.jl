a9c942ae919743bbc075cf4dba78c3033698b646
Ethan Meitz <54505069+ejmeitz@users.noreply.github.com>
2026-02-20 14:14:16 -0500

    dasd

--- workflows/neon_lattice_params_stdep.jl
+++ workflows/neon_lattice_params_stdep.jl
@@ -1,19 +1,15 @@
 # Runs sTDEP for all the lattice parameter and temperature combinations
 
-quantum = true
-base_outpath = "/mnt/merged/emeitz/CumulantAnalysisTest/Neon_ThermalExpansion_IFCs_sTDEP"
-getoutpath = (T) -> joinpath(base_outpath,  "T$(T)")
-
 Ts = [4,6,8,10,12,14,16,18,20,22,24]
 pot_cmds = ["pair_style lj/cut 6.955", "pair_coeff * * 0.0032135 2.782", "pair_modify shift yes"]
 
-
+quantum = true
 n_lattice_params = 8
 n_iter = 9
-maximum_frequency = 2.5
+maximum_frequency = 2.5 # THz
 
+base_outpath = "/mnt/merged/emeitz/CumulantAnalysisTest/Neon_ThermalExpansion_IFCs_sTDEP"
 structures_basepath = (T,a) = "/mnt/merged/emeitz/CumulantAnalysisTest/thermal_expansion_series/T$(T)/a$(a)"
-
 
 for i in 1:n_lattice_params
 
@@ -23,6 +19,7 @@ for i in 1:n_lattice_params
         sb = structures_basepath(T, i)
         outpath_T = joinpath(outpath_a, "T$(T)")
         mkpath(outpath_T)
+
         make_stdep_ifcs(
             joinpath(sb, "infile.ucposcar"),
             joinpath(sb, "infile.ucposcar"),
