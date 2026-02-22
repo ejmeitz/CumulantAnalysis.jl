59bf583dc81feeb7f6e013ed9a41d9dccb19da38
Ethan Meitz <54505069+ejmeitz@users.noreply.github.com>
2026-02-21 21:32:15 -0500

    updates

--- workflows/neon_lattice_params.jl
+++ workflows/neon_lattice_params.jl
@@ -5,21 +5,22 @@ nconf = 100_000
 nboot = 5000
 
 quantum = true
-base_outpath = "/mnt/merged/emeitz/CumulantAnalysisTest/Neon_ANALYTICAL_PIMD"
-getoutpath = (T) -> joinpath(base_outpath,  "T$(T)")
+base_outpath = "/mnt/merged/emeitz/CumulantAnalysisTest/Neon_ThermalExpansion_Runs"
 
 Ts = [4,6,8,10,12,14,16,18,20,22,24]
 pot_cmds = ["pair_style lj/cut 6.955", "pair_coeff * * 0.0032135 2.782", "pair_modify shift yes"]
 
-for a in trial_lattice_constants
+for (i,a) in enumerate(trial_lattice_constants)
 
     ## sTDEP IFCs
-    ifc_basepath = (T) -> "/mnt/merged/emeitz/Neon_sTDEP/results/T$(T)"
+    ifc_basepath = (T) -> "/mnt/merged/emeitz/CumulantAnalysisTest/Neon_ThermalExpansion_IFCs_sTDEP/a$(i)/T$(T)/iter009"
     ucposcar_path = (T) -> joinpath(ifc_basepath(T), "infile.ucposcar")
     ssposcar_path = (T) -> joinpath(ifc_basepath(T), "infile.ssposcar")
     ifc2_path = (T) -> joinpath(ifc_basepath(T), "infile.forceconstant")
     ifc3_path = (T) -> joinpath(ifc_basepath(T), "infile.forceconstant_thirdorder")
     ifc4_path = (T) -> joinpath(ifc_basepath(T), "infile.forceconstant_fourthorder")
+
+    getoutpath = (T) -> joinpath(base_outpath, "a$(i)", "T$(T)")
 
     crystal_thermodynamic_properties(
         Ts,
