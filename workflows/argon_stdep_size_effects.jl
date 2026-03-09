fe252baf0f294d96df4e22f835eab1e5ed54408b
Ethan Meitz <54505069+ejmeitz@users.noreply.github.com>
2026-03-09 13:15:49 -0400

    fix

--- workflows/argon_stdep_size_effects.jl
+++ workflows/argon_stdep_size_effects.jl
@@ -9,10 +9,11 @@ Ns = [5,6] # already have 4 unitcell data
 quantum = false
 n_iter = 10
 maximum_frequency = 2.5 # THz
+r_cut = 8.5
 
 base_outpath = (N) -> "/mnt/merged/emeitz/CumulantAnalysisTest/LJ_sTDEP_SIZE_EFFECTS/N$(N)_T$(T)"
 
-pot_cmds = ["pair_style lj/cut 8.5", "pair_coeff * * 0.010423 3.4", "pair_modify shift yes"]
+pot_cmds = ["pair_style lj/cut $(r_cut)", "pair_coeff * * 0.010423 3.4", "pair_modify shift yes"]
 
 structure_basepath = (N) -> "/home/emeitz/scripts/TDEP/LJ/size_effects/80K/$(N)UC"
 
