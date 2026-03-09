84bc7eb96a82689fef2421d7077135059969e33c
Ethan Meitz <54505069+ejmeitz@users.noreply.github.com>
2026-03-09 13:14:04 -0400

    fixes

--- workflows/argon_stdep_size_effects.jl
+++ workflows/argon_stdep_size_effects.jl
@@ -12,17 +12,15 @@ maximum_frequency = 2.5 # THz
 
 base_outpath = (N) -> "/mnt/merged/emeitz/CumulantAnalysisTest/LJ_sTDEP_SIZE_EFFECTS/N$(N)_T$(T)"
 
-ucposcar_path = (T) -> "/mnt/merged/emeitz/LJ_IFC_INTERPOLATION_NODES_FINE/infile.ucposcar"
-ssposcar_path = (T) -> "/mnt/merged/emeitz/LJ_IFC_INTERPOLATION_NODES_FINE/infile.ssposcar" #4UC
 pot_cmds = ["pair_style lj/cut 8.5", "pair_coeff * * 0.010423 3.4", "pair_modify shift yes"]
 
 structure_basepath = (N) -> "/home/emeitz/scripts/TDEP/LJ/size_effects/80K/$(N)UC"
 
-for T in Ts
+for N in Ns
     make_stdep_ifcs(
-        joinpath(structure_basepath, "infile.ucposcar"),
-        joinpath(structure_basepath, "infile.ssposcar"),
-        base_outpath(T),
+        joinpath(structure_basepath(N), "infile.ucposcar"),
+        joinpath(structure_basepath(N), "infile.ssposcar"),
+        base_outpath(N),
         pot_cmds,
         n_iter,
         r_cut,
